11a93b70e1c7f842fd0b3f7e4f46a7900c51b273
Gordus Alexander <aldrmiller@gmail.com>
2024-06-01 13:59:05 +0600

    реализовал внешний вид приложения. Вывел список карточек на страницу

--- src/App.tsx
+++ src/App.tsx
@@ -7,38 +7,62 @@ import coffeeImage from './assets/ic-coffee.png';
 import lemonadeImage from './assets/ic-lemonade.png';
 import colaImage from './assets/ic-cola.png';
 import {OrderList} from './types';
+import CardMenu from './components/CardMenu/CardMenu';
 
-interface CardMenu {
+interface MenuItem {
   name: string;
   price: number;
   image: string;
 }
 
-const cardMenu: CardMenu[] = [
+const menuItem: MenuItem[] = [
   {name: 'Hamburger', price: 180, image: burgerImage},
-  {name: 'HotDog', price: 80, image: hotDogImage},
+  {name: 'Lemonade', price: 120, image: lemonadeImage},
   {name: 'Fries', price: 60, image: friesImage},
   {name: 'Coffee', price: 70, image: coffeeImage},
-  {name: 'Lemonade', price: 120, image: lemonadeImage},
+  {name: 'HotDog', price: 80, image: hotDogImage},
   {name: 'Cola', price: 50, image: colaImage},
 ];
 
 const App = () => {
   const [orderList, setOrderList] = useState<OrderList[]>([
     {name: 'Hamburger', count: 0},
-    {name: 'HotDog', count: 0},
+    {name: 'Lemonade', count: 0},
     {name: 'Fries', count: 0},
     {name: 'Coffee', count: 0},
-    {name: 'Lemonade', count: 0},
+    {name: 'HotDog', count: 0},
     {name: 'Cola', count: 0},
   ]);
 
+  const click = () => {
+    console.log('click');
+  };
 
 
   return (
-    <>
+    <div className="constructor-container">
+      <fieldset className="order-list">
+        <legend className="constructor-name">Order Details:</legend>
+      </fieldset>
 
-    </>
+
+      <fieldset className="card-menu-list">
+        <legend className="constructor-name">Add items:</legend>
+        <div className="card-menu-wrapper">
+          {menuItem.map((card,index) => {
+            return (
+              <CardMenu
+                key={`card-${index}`}
+                image={card.image}
+                name={card.name}
+                price={card.price}
+                onClickCard={() => click()}
+              />
+            );
+          })}
+        </div>
+      </fieldset>
+    </div>
   );
 };
 
--- src/components/CardMenu/CardMenu.tsx
+++ src/components/CardMenu/CardMenu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './CardMenu.css';
 
 interface CardProps {
   image: string;
